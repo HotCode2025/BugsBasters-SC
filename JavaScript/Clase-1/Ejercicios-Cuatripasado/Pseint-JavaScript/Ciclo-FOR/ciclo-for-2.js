4ba7493e4f53367761cae782604dada135ac6692
Lourdes Eschler <lourdeseschler@gmail.com>
2025-08-28 19:38:13 -0300

    Update ciclo-for-2.js

--- JavaScript/Clase-1/Ejercicios-Cuatripasado/Pseint-JavaScript/Ciclo-FOR/ciclo-for-2.js
+++ JavaScript/Clase-1/Ejercicios-Cuatripasado/Pseint-JavaScript/Ciclo-FOR/ciclo-for-2.js
@@ -1,1 +1,16 @@
+// Se desea calcular independientemente la suma de los números pares e impares entre 1 y 50
+
+let suma_pares = 0;
+let suma_impares = 0;
+
+for (let i = 2; i <= 49; i++) {
+    if (i % 2 === 0) {
+        suma_pares += i;
+    } else {
+        suma_impares += i;
+    }
+}
+
+console.log("La suma de pares es: " + suma_pares);
+console.log("La suma de impares es: " + suma_impares);
 
