43d4c63c18dc6711dfaf701304bd2bebf8d06492
Lourdes Eschler <lourdeseschler@gmail.com>
2025-08-28 19:37:11 -0300

    Update ciclo-for-1.js
    primer ejercicio de pseint con el ciclo for

--- JavaScript/Clase-1/Ejercicios-Cuatripasado/Pseint-JavaScript/Ciclo-FOR/ciclo-for-1.js
+++ JavaScript/Clase-1/Ejercicios-Cuatripasado/Pseint-JavaScript/Ciclo-FOR/ciclo-for-1.js
@@ -1,1 +1,14 @@
+// Calcular la suma de los "N" primeros números
+
+// Pedimos el valor al usuario
+let N = parseInt(prompt("Digite la cantidad de números a sumarse: "));
+
+let suma = 0;
+
+// Bucle para sumar del 1 hasta N
+for (let i = 1; i <= N; i++) {
+    suma = suma + i;  // suma += i;
+}
+
+alert("La suma es: " + suma);
 
