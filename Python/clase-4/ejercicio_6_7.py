a6dcf6fe98d7b600a680a213e9b4dc0125009fc6
tobias andres carbajal <tobycarbajal43@gmail.com>
2025-09-01 20:53:41 -0300

    clase 4 ejercicios 6,7

---
+++ Python/clase-4/ejercicio_6_7.py
@@ -0,0 +1,6 @@
+# ejercicio numero 6 tabla de multiplicar
+n = int(input("¿Introduce el número del cual quieres generar la tabla?: "))
+
+for i in range(1, 11):
+    print(f'{n} x {i} = {n*i}') 
+#ejercicio numero 7 juego adivina el numero
