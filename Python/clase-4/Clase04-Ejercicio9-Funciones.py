76899fd0e27086aec4fb50977bd88c40da764339
Ian Ditlevsen <ian.ditlevsen.2011@gmail.com>
2025-09-01 20:14:58 -0300

    Subido el ejercicio 9 de la clase 4 de funciones

---
+++ Python/clase-4/Clase04-Ejercicio9-Funciones.py
@@ -0,0 +1,8 @@
+# Ejercicio 9
+frase = input("Ingrese una frase: ")
+
+frase_sin_espacios = frase.replace(" ", "")
+
+print("Frase final:", frase_sin_espacios)
+
+print("N° de caracteres:", len(frase_sin_espacios))
