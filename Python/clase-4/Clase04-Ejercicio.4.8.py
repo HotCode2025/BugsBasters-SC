a6a188ed3eafb22c3e358e2f953ef7d74eae2f0c
Lourdes Eschler <lourdeseschler@gmail.com>
2025-09-01 21:27:56 -0300

    Update Clase04-Ejercicio.4.8.py
    ejercicio 4.7 de la clase 4 de python

--- Python/clase-4/Clase04-Ejercicio.4.8.py
+++ Python/clase-4/Clase04-Ejercicio.4.8.py
@@ -1,1 +1,23 @@
+# Insertar elementos y ordenarlos
+# Pedir números y meterlos en una lista, cuando el usuario introduzca un número 0
+# nuestro programa dejaría de insertar.
+# Por ultimo, mostrar los números ordenados de menor a mayor
+
+#Crear lista vacía
+numeros = []
+
+print("Ingrese números (0 para terminar):")
+
+# Pedir números al usuario
+while True:
+    num = int(input("Número: "))
+    if num == 0:  # condición de salida
+        break
+    numeros.append(num)
+
+# Ordenar la lista
+numeros.sort()
+
+# Mostrar números ordenados
+print("Números ordenados de menor a mayor:", numeros)
 
