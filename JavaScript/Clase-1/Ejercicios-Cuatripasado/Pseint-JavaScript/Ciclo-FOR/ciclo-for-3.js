783579420ff4feab80ad77776feea7076d3eb241
Lourdes Eschler <lourdeseschler@gmail.com>
2025-09-02 10:51:13 -0300

    Update ciclo-for-3.js
    documentación del código

--- JavaScript/Clase-1/Ejercicios-Cuatripasado/Pseint-JavaScript/Ciclo-FOR/ciclo-for-3.js
+++ JavaScript/Clase-1/Ejercicios-Cuatripasado/Pseint-JavaScript/Ciclo-FOR/ciclo-for-3.js
@@ -1,12 +1,15 @@
 const numeros = [5, -3, 0, 8, -1, 2, 0, -7, 10, 0]; // ejemplo de 10 números
 
+//definimos las variables
 let conteoPositivos = 0;
 let conteoNegativos = 0;
 let conteoNeutros = 0;
 
+//utilizamos el ciclo for para contar los numeros
 for (let i = 0; i < numeros.length; i++) {
   let num = numeros[i];
 
+  //los dividimos en neutros, positivos y negativos
   if (num === 0) {
     conteoNeutros++;
   } else if (num > 0) {
@@ -16,6 +19,7 @@ for (let i = 0; i < numeros.length; i++) {
   }
 }
 
+//mostramos los resultados
 console.log("Resultados:");
 console.log("Positivos:", conteoPositivos);
 console.log("Negativos:", conteoNegativos);
