3193a35f4e819fb094a2e2040e0162e3befb3bef
Brian Sastre <briansastre159@gmail.com>
2025-09-24 17:34:11 -0300

    Ejercicios de la clase 7 de python , incluyendo la tarea que se subio ayer que serian los ejercicios 9.5 y 9.6

---
+++ Python/clase-7/Clase07-Ejercicios9.1.py
@@ -0,0 +1,36 @@
+# Leccion6 - Persona.py
+
+class Persona: # Creamos una clase
+    
+    def __init__(self, nombre, apellido, edad): # Se le llama metodo Init Dunder
+        self.nombre = nombre
+        self.apellido = apellido
+        self.edad = edad
+        
+    def mostrar_detalle(self): # self es igual a this
+        print(f'Persona: {self.nombre} {self.apellido} {self.edad}')
+
+# ---
+
+# 
+persona1 = Persona('Ariel', 'Betancud', 40) # Necesitamos enviar argumentos
+print(persona1.nombre) # Tarea: Hacer el print igual que con el objeto 2
+print(persona1.apellido)
+print(f'El objeto1 de la clase persona: {persona1.nombre} {persona1.apellido} Su edad es: {persona1.edad}')
+persona2 = Persona('Osvaldo', 'Giordanini', 45)
+print(f'El objeto2 de la clase persona: {persona2.nombre} {persona2.apellido} Su edad es: {persona2.edad}')
+
+# ---
+
+# 
+persona1.nombre = 'Liliana'
+persona1.apellido = 'Buccella'
+persona1.edad = 40
+print(f'El objeto1 modificado de la clase persona: {persona1.nombre} {persona1.apellido} Su edad es: {persona1.edad}')
+
+# Los atributos son: caracteristicas
+# Los metodos son: el comportamiento que van a tener los objetos (acciones)
+persona1.mostrar_detalle() # La referencia en este caso se pasa de manera automatica
+persona2.mostrar_detalle()
+
+# Persona.mostrar_detalle(persona1) # Debemos pasarle una referencia para el self o dara error
