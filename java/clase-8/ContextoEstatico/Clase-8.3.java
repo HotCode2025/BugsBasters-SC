098d04f264c4c6698ee1f109748c02ef8373bbd2
Candelaria Ribotta <candelariaribotta385@gmail.com>
2025-10-01 20:25:10 -0300

    Create Clase-8.3.java
    Clase 8 video 3

---
+++ java/clase-8/ContextoEstatico/Clase-8.3.java
@@ -0,0 +1,9 @@
+// toString en Persona
+public String toString(){ // Convierte en una cadena cada atributo
+    return "Persona [ nombre: "+this.nombre+
+            ", sueldo: "+this.sueldo+
+            ", eliminado: "+this.eliminado+" ]";
+}
+
+// pasamos el toString a la clase PersonaPrueba
+// System.out.println("persona1 = " + persona1);
