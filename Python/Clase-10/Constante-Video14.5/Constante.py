83b8e7081fef081bc4beff08d14965c82ee45aee
Brian Sastre <briansastre159@gmail.com>
2025-11-03 16:47:21 -0300

    Clase 10 siguiendo el orden del campues , siguiendo las practicas y mejorando los codigos. Dentro  de la carpte clase 10 hay otra carpeta ya que usa las constantes y asi es mas facil llamarlas

---
+++ Python/Clase-10/Constante-Video14.5/Constante.py
@@ -0,0 +1,7 @@
+#constantes en python
+#Clase 10 Ejercicio 14.5 
+class Matematica:
+    PI = 3.1416
+    
+
+Mi_Constante = 'esta es una variable constante  '
---
+++ Python/Clase-10/Constante-Video14.5/Test_Constante.py
@@ -0,0 +1,8 @@
+from Constante import *
+from Constante import Matematica as Mate
+print(Mi_Constante)
+
+Mi_Constante = 'Nuevo valor'
+print(Mi_Constante)
+
+print(Mate)
