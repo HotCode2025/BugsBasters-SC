e0b737bd15488b93ea9611fae140f15d59e55500
Rodrigo Funes <76707339+RodrigoFunes@users.noreply.github.com>
2025-11-19 13:22:47 -0300

    Prueba de errores y validacion

--- Python/Clase-12/test-polimorfismo.py
+++ Python/Clase-12/test-polimorfismo.py
@@ -5,6 +5,8 @@ def imprimir_detalles(objeto):
     # print(objeto) #De manera indirecta llama al str de la clase Empleado o Gerente
     print(type(objeto)) #Esto es para saber el tipo de dato que recibe
     print(objeto.mostrar_detalles)
+    if insistance(objeto, Gerente):
+        print(objeto.departamento)
 
 empleado = Empleado('Ariel', 50000)
 imprimir_detalle(empleado)
